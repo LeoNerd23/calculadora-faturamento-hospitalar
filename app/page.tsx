51f6c012706f49ec47afe44b2467718f823fdc9d
Leonardo Alcantara <leonardoalc23@gmail.com>
2025-05-29 16:47:45 -0300

    update vercel file and app config

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import MedicalFeesForm from "../components/medical-fees-form"
 
-export default function Page() {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6">
       <div className="w-full max-w-4xl">

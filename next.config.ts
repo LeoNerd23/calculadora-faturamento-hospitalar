a0f9173fcb715f9280e39c61d57a5df3d98e6c58
Leonardo Alcantara <leonardoalc23@gmail.com>
2025-05-29 16:51:29 -0300

    update app config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // habilita o App Router
-  },
 }
 
 module.exports = nextConfig
